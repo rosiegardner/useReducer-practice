759d931c44717f7ddf0b30154986075ab221b02b
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-11 11:24:28 -0700

    invoke userReducer hook

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const initalState = {
+  counter: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return{
+        counter: state.counter + 1
+      };
+      default:
+        throw new Error(`There is no action matching ${action.type}.`);
+  }
+}
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    document.title = state.counter;
+  }, [state.counter]);
+
+  return (
+    <React.Fragment>
+      { hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1> }
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
+      <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
+    </React.Fragment>
+  );
+}
+
+export default Counter;
